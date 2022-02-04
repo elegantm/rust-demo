f5a64885df2f7281281d68665fda35aa037e44ae
elegantm <elegantm@elegantmdeMacBook-Pro.local>
2022-02-04 21:08:56 +0800

    fix bug

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,30 +3,41 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Hello, world!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("the secret number is: {} ", secret_number);
+    println!("The secret number is: {}", secret_number);
+    println!("Guess the number!");
 
     loop {
-        println!("please input your guess.");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        println!("Please input your guess.");
 
-        let guess: u32 = guess.trim().parse(){
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("failed read line");
+
+        println!("You guessed: {}", guess);
+    
+        let parse_result = guess.trim().parse();
+        let guess: u32 = match parse_result {
+            // If parse succeeded evaluate to the number
             Ok(num) => num,
-            Err(_) => continue,
+            // If parse failed repeat the loop
+            Err(msg) => {
+                println!("input error : {}", msg);
+                continue;
+            },
         };
 
-        println!("your guessed: {}", guess);
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
+            Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("you win!");
+                println!("You win!");
                 break;
             }
-        };
+
+        }
+    
     }
+    
 }
