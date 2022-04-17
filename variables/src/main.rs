2e88914b1ad46c775d03316f2b4e5804d0cc992b
elegantm <starelegant@163.com>
2022-04-17 21:29:35 +0800

    add rust advantage and disadvantage

---
+++ variables/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    println!("Hello, world!");
+    let x = 5;
+    println!("x = {}", x);
+    let y : i32 =592470;
+    println!("y = {}", y);
+    let x = 2.1;
+    let y : f32 = 3.1400;
+    println!("x = {}", x);
+    println!("y = {}", y);
+
+    let x =  2.0 +  y as f64;
+    println!("x = {}", x);
+
+
+    // sum 5+3
+    let sum = 5 + 3;
+    println!("sum = {}", sum);
+
+    // diffrence 5-3
+    let diffrence = 5 - 3;
+    println!("diffrence = {}", diffrence);
+
+    // product 5*3
+    let product = 5 * 3;
+    println!("product = {}", product);
+
+    // division 5/3
+    let division = 5 / 3;
+    println!("division = {}", division);
+
+    // remainder of division 43%5
+    let remainder = 43 % 5;
+    println!("remainder = {}", remainder);
+
+    // bool false
+    let t : bool = false;
+    println!("t = {}", t);
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("c = {}, z = {}, heart_eyed_cat = {}", c, z, heart_eyed_cat);
+
+    
+}
