8d368b56a8bf17a73f135c840d12b850cd9296cb
elegantm <18309699+elegantm@users.noreply.github.com>
2024-09-10 22:53:56 +0800

    add demo for cli time

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+const VERSION: &str = "1.0.0";
 fn main() {
     println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    // Check if any arguments are provided
+    if args.len() < 2 {
+        eprintln!("Usage: {} [-v | -t]", args[0]);
+        std::process::exit(1);
+    }
+
+    // Handle the arguments
+    match args[1].as_str() {
+        "-v" => {
+            println!("Version: {}", VERSION);
+        },
+        "-t" => {
+            let start = SystemTime::now();
+            match start.duration_since(UNIX_EPOCH) {
+                Ok(n) => println!("Current time (seconds since UNIX EPOCH): {}", n.as_secs()),
+                Err(_) => eprintln!("SystemTime before UNIX EPOCH!"),
+            }
+        },
+        _ => {
+            eprintln!("Invalid option: {}", args[1]);
+            eprintln!("Usage: {} [-v | -t]", args[0]);
+            std::process::exit(1);
+        }
+    }
+   
+
 }
